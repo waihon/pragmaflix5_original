f4ca1da43a7582fa0c400621ee640e20f3b4be21
Waihon Yew <yewwaihon@gmail.com>
2016-10-13 22:17:32 +0800

    Initial commit

---
+++ config/application.rb
@@ -0,0 +1,21 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module Pragmaflix5
+  class Application < Rails::Application
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
+
+    # Make sure that we always have a database connection established
+    # when we're in the Rails console.
+    console do
+      ActiveRecord::Base.connection
+    end
+  end
+end
