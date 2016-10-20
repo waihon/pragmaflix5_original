8600be73f21ced18eb2e9efc94d28267af35cd22
Waihon Yew <yewwaihon@gmail.com>
2016-10-20 19:34:13 +0800

    Complete 7. Helpers

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,2 +1,11 @@
 module MoviesHelper
+  def format_total_gross(movie)
+    if movie.flop?
+      # "<strong>Flop!</strong>".html_safe
+      # or
+      content_tag(:strong, "Flop!")
+    else
+      number_to_currency(movie.total_gross)
+    end
+  end
 end
---
+++ config/initializers/date_formats.rb
@@ -0,0 +1,1 @@
+Date::DATE_FORMATS[:released_on] = "%B %e, %Y"
